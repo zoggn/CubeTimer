bf1128904d2776a73df9f92d2e2427e3805ba54d
Nikita <kurkov.nikita@gmail.com>
2019-01-04 22:19:17 +0200

    Initial implementation for timer(display, button)
    Just pin definitions

--- CubeTimer.ino
+++ CubeTimer.ino
@@ -1,20 +1,37 @@
 #include "src/ScrambleTools.h"
 
-/* Settings */
-#define scrSize 20 //Define scramble size fo 3x3
+/* Scramble settings */
+  #define scrSize 20 //Define scramble size fo 3x3
+  Scrambles newScr;
 /* End Settings */
 
-Scrambles newScr;
 
+/* Timer variables */
+  boolean statusFlag = false;
+
+/* End */
+
+
+
+/* Misc definitions */
+  #define firstButt 16
+  #define secondButt 5
+  boolean buttonState = false;
+/* End */
+
+/* Disp Settings */
+  #define SDA 4
+  #define SCL 0
+/* End */
 void setup() {
   Serial.begin(115200);
-    randomSeed(analogRead(0));
-
+    //randomSeed(analogRead(0));
+  pinMode(firstButt,INPUT_PULLUP);
+  pinMode(secondButt,INPUT_PULLUP);
 }
 
 void loop() {
-  delay(10000);
-  Serial.println(newScr.getScramble(scrSize));
+  delay(5000);
 
 
 }
